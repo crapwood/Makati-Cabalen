b7d2bdccc70f2c54dd6f8be650ea2b9aec7ee927
james <jjbe.93@gmail.com>
2020-04-20 23:45:38 +0300

    added route on home

--- mc_supermarket/src/App.js
+++ mc_supermarket/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import Home from "./pages/homepage/index";
 
 function App() {
   return (
