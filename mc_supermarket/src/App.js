e47c1c191d1e6dd4d0a303359c5748bf1a661498
james <jjbe.93@gmail.com>
2020-04-20 23:51:38 +0300

    added nav on router

--- mc_supermarket/src/App.js
+++ mc_supermarket/src/App.js
@@ -3,13 +3,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import Home from "./pages/homepage/index";
+import Nav from "./pages/navbar/navbar";
 
 function App() {
   return (
     <>
       <Router>
         <div>
-          {/* <Nav /> */}
+          <Nav />
           <Switch>
             <Route exact path="/" component={Home} />
           </Switch>
