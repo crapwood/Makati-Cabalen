309870410776d244329867d870d344078641b2a3
james <jjbe.93@gmail.com>
2020-04-20 23:44:57 +0300

    added router on app.js

--- mc_supermarket/src/App.js
+++ mc_supermarket/src/App.js
@@ -1,9 +1,22 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Router>
+        <div>
+          {/* <Nav /> */}
+          <Switch>
+            <Route exact path="/" component={Home} />
+          </Switch>
+          {/* <Footer /> */}
+        </div>
+      </Router>
+    </>
+  );
 }
 
 export default App;
